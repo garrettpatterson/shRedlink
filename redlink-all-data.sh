6c1e81e0824bdfae2cbd82ec989a3909d460a910
Garrett Patterson <garrett.patterson@gmail.com>
2016-05-17 13:20:20 -0700

    adding fanMode and statusHeat flags

--- redlink-all-data.sh
+++ redlink-all-data.sh
@@ -4,8 +4,8 @@
 #echo $PASSWORD
 
 if [ ! -f tempdata.log ] 
-	then curl -sL -c cookie-jar.txt --data "UserName=$USERNAME&Password=$PASSWORD&timeOffset=420" https://mytotalconnectcomfort.com/portal/  | grep -o "[A-Za-z]\{1,\}, '\?[0-9]\{2,3\}\.\?[0-9]\{0,4\}'\?" | grep -w 'dispTemperature\|weatherTemperature\|heatSetpoint' | awk -F", " '{print $1}' | tr -s '\n' '\t' | sed -e "s/'//g" | awk -F "" '{print "timestamp\t"$0}' > tempdata.log ;
+	then curl -sL -c cookie-jar.txt --data "UserName=$USERNAME&Password=$PASSWORD&timeOffset=420" https://mytotalconnectcomfort.com/portal/  | grep -o "[A-Za-z]\{1,\}, '\?[0-9]\{1,3\}\.\?[0-9]\{0,4\}'\?" | grep -w 'dispTemperature\|weatherTemperature\|heatSetpoint\|fanMode\|statusHeat' | awk -F", " '{print $1}' | tr -s '\n' '\t' | sed -e "s/'//g" | awk -F "" '{print "timestamp\t"$0}' > tempdata.log ;
 fi
 
-curl -sL -c cookie-jar.txt --data "UserName=$USERNAME&Password=$PASSWORD&timeOffset=420" https://mytotalconnectcomfort.com/portal/  | grep -o "[A-Za-z]\{1,\}, '\?[0-9]\{2,3\}\.\?[0-9]\{0,4\}'\?" | grep -w 'dispTemperature\|weatherTemperature\|heatSetpoint' | awk -F", " '{print $2}' | tr -s '\n' '\t' | sed -e "s/'//g" | awk -F "" -v d=$(date +%s) '{print d"\t"$0}' >> tempdata.log
+curl -sL -c cookie-jar.txt --data "UserName=$USERNAME&Password=$PASSWORD&timeOffset=420" https://mytotalconnectcomfort.com/portal/  | grep -o "[A-Za-z]\{1,\}, '\?[0-9]\{1,3\}\.\?[0-9]\{0,4\}'\?" | grep -w 'dispTemperature\|weatherTemperature\|heatSetpoint\|fanMode\|statusHeat' | awk -F", " '{print $2}' | tr -s '\n' '\t' | sed -e "s/'//g" | awk -F "" -v d=$(date +%s) '{print d"\t"$0}' >> tempdata.log
   
